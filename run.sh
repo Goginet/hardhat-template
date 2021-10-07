5e472688d81903d90a4d4589bdc48612d7f803e8
George <georgy.schapchits@synesis.ru>
2021-10-07 16:32:43 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -21,10 +21,6 @@ shell2http -basic-auth='unfedral:b3e08d86cb7da358' -host='0.0.0.0' -port=8080 -f
     /api/set_borrow_paused 'npx hardhat  --network localhost setBorrowPaused $v_address $v_value' \
     /api/set_reserve_factor 'npx hardhat  --network localhost setReserveFactor $v_value' \
     /api/get_reserve_factor 'npx hardhat  --network localhost getReserveFactor' \
-
-
-
-
     /api/restart 'kill -9 $(pgrep node)' &
 
 afterRun &
